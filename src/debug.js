b94eb1c0b3f4c3227cd46268780e391ec7cb67e2
adamjace <adamjace@gmail.com>
2017-05-05 06:05:58 +1000

    add debug.js

---
+++ src/debug.js
@@ -0,0 +1,34 @@
+'use strict'
+
+require('dotenv').config()
+
+const Timer = require('./lib/timer')
+const Logger = require('./lib/logger')
+const draw = require('./lib/draw')
+const { run } = require('./lib/utils')
+
+const timer = new Timer()
+
+const fixture = {
+  id: '__test',
+  index: 1,
+  matchDay: 35,
+  home: {
+    team: 'Man United',
+    score: 4
+  },
+  away: {
+    team: 'Liverpool',
+    score: 2
+  }
+}
+
+const startWorker = async () => {
+  timer.start()
+  Logger.log('info', 'Starting debug worker')
+  await run(draw, fixture)
+  timer.stop()
+  Logger.log('info', timer.report)
+}
+
+startWorker()
