59ccfad65144aded7ab3edc72e5acc163b820157
adamjace <adamjace@gmail.com>
2017-08-14 07:28:14 +1000

    update premier league ID for 17/18 season

--- src/competitions/premierLeague.js
+++ src/competitions/premierLeague.js
@@ -1,128 +1,108 @@
 'use strict'
 
 module.exports = {
-  id: 426,
+  id: 445,
   tags: '#epl #premierleague',
   teams: [
     {
-      name: 'Hull City FC',
-      short: 'Hull City',
-      tags: '#hull #hullfc',
-      crest: 'hull.png'
+      name: 'Newcastle United FC',
+      short: 'Newcastle',
+      tags: '#newcastle #newcastleunitedfc'
     },
     {
       name: 'Leicester City FC',
       short: 'Leicester',
-      tags: '#leicester #leicestercity',
-      crest: 'leicester.png'
+      tags: '#leicester #leicestercity'
     },
     {
       name: 'Southampton FC',
       short: 'Southampton',
-      tags: '#southampton #southamptonfc',
-      crest: 'southampton.png'
+      tags: '#southampton #southamptonfc'
     },
     {
       name: 'Watford FC',
       short: 'Watford',
-      tags: '#watford #watfordfc',
-      crest: 'watford.png'
+      tags: '#watford #watfordfc'
     },
     {
-      name: 'Middlesbrough FC',
-      short: 'Middlesbrough',
-      tags: '#middlesbrough #middlesbroughfc',
-      crest: 'middlesbrough.png'
+      name: 'Huddersfield Town',
+      short: 'Huddersfield',
+      tags: '#muddersfieldtown #muddersfield'
     },
     {
       name: 'Stoke City FC',
       short: 'Stoke',
-      tags: '#stoke #stokecity',
-      crest: 'stoke.png'
+      tags: '#stoke #stokecity'
     },
     {
       name: 'Everton FC',
       short: 'Everton',
-      tags: '#everton #evertonfc',
-      crest: 'everton.png'
+      tags: '#everton #evertonfc'
     },
     {
       name: 'Tottenham Hotspur FC',
       short: 'Spurs',
-      tags: '#tottenham #tottenhamhotspur #spurs',
-      crest: 'tottenham.png'
+      tags: '#tottenham #tottenhamhotspur #spurs'
     },
     {
       name: 'Crystal Palace FC',
       short: 'Crystal Palace',
-      tags: '#crystalpalace #crystalapalacefc',
-      crest: 'crystalpalace.png'
+      tags: '#crystalpalace #crystalapalacefc'
     },
     {
       name: 'West Bromwich Albion FC',
       short: 'West Brom',
-      tags: '#westbromwich #westbromwichalbion',
-      crest: 'westbrom.png'
+      tags: '#westbromwich #westbromwichalbion'
     },
     {
       name: 'Burnley FC',
       short: 'Burnley',
-      tags: '#burnley #burnleyfc',
-      crest: 'burnley.png'
+      tags: '#burnley #burnleyfc'
     },
     {
       name: 'Swansea City FC',
       short: 'Swansea',
-      tags: '#swansea #swanseacity',
-      crest: 'swansea.png'
+      tags: '#swansea #swanseacity'
     },
     {
       name: 'Manchester City FC',
       short: 'Man City',
-      tags: '#manchestercity #mancity',
-      crest: 'mancity.png'
+      tags: '#manchestercity #mancity'
     },
     {
-      name: 'Sunderland AFC',
-      short: 'Sunderland',
-      tags: '#sunderland #sunderlandafc',
-      crest: 'sunderland.png'
+      name: 'Brighton & Hove Albion',
+      short: 'Brighton',
+      tags: '#brighton #brightonhove'
     },
     {
       name: 'AFC Bournemouth',
       short: 'Bournemouth',
-      tags: '#bournemouth #afcbournemouth',
-      crest: 'bournemouth.png'
+      tags: '#bournemouth #afcbournemouth'
     },
     {
       name: 'Manchester United FC',
       short: 'Man United',
-      tags: '#manchesterunited #manchesterutd #manunited #reddevils',
-      crest: 'manunited.png'
+      tags: '#manchesterunited #manchesterutd #manunited #reddevils'
     },
     {
       name: 'Arsenal FC',
       short: 'Arsenal',
-      tags: '#arsenal #arsenalfc #gunners',
-      crest: 'arsenal.png'
+      tags: '#arsenal #arsenalfc #gunners'
     },
     {
       name: 'Liverpool FC',
       short: 'Liverpool',
-      tags: '#liverpool #liverpoolfc #lfc #ynwa',
-      crest: 'liverpool.png'
+      tags: '#liverpool #liverpoolfc #lfc #ynwa'
     },
     {
       name: 'Chelsea FC',
       short: 'Chelsea',
-      tags: '#chelsea #chelseafc',
-      crest: 'chelsea.png'
+      tags: '#chelsea #chelseafc'
     },
     {
       name: 'West Ham United FC',
       short: 'West Ham',
-      tags: '#westham #westhamunited',
-      crest: 'westham.png'
+      tags: '#westham #westhamunited'
     }
   ]
 }
